4b1bef83e949f867ef28736bc4950eaded10d2b4
tirea02 <tirea02@gmail.com>
2023-09-06 11:36:30 +0900

    DB 관련 코드 전부 주석처리 (AWS 테스트 위함)

--- src/main/java/com/net/common/rs/entity/Guest.java
+++ src/main/java/com/net/common/rs/entity/Guest.java
@@ -1,20 +1,20 @@
-package com.net.common.rs.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Data
-@Table(name = "guests")
-public class Guest {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String username;
-    private String email;
-    private String password;
-
-    // Constructors, getters, setters, and other fields
-}
+//package com.net.common.rs.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+////@Entity
+//@Data
+////@Table(name = "guests")
+//public class Guest {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String username;
+//    private String email;
+//    private String password;
+//
+//    // Constructors, getters, setters, and other fields
+//}
