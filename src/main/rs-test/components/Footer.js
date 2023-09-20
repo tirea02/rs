c0272dc4ee6aa5a582b3b2eed22e772ccab357d0
tirea02 <tirea02@gmail.com>
2023-09-20 17:37:19 +0900

    footere css tailwind 방식으로 수정 및 추가

--- src/main/rs-test/components/Footer.js
+++ src/main/rs-test/components/Footer.js
@@ -1,44 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Script from 'next/script';
-import Head from "next/head"
-
-import styles from '../src/css/Footer.module.css';
+import Head from "next/head";
 
 const Footer = () => {
     return (
-        <footer className={styles.myFooter}>
-            <div className="footer-inner">
-                <div className="footer-logo">
-                    <img src="../public/images/Footerlogo.png" alt="#none" />
+        <footer className="bg-gray-900 text-white py-8">
+            <div className="container mx-auto flex flex-col gap-8 p-8">
+                <div className="flex items-center justify-center">
+                    <img src="../public/images/Footerlogo.png" alt="#none" className="w-52 h-14" />
                 </div>
-                <div className="copyright">
+                <div className="flex flex-col md:flex-row justify-between">
                     <div className="service">
                         <p>
                             Service center: 111 - 1111
                             <span>
-                Weekdays: 09:00 am to 6:00 pm<br />
-                Lunch time: 12:00 am to 1:00 pm Closed<br />
-                on Sat/Sun/Holidays
-              </span>
+                                Weekdays: 09:00 am to 6:00 pm<br />
+                                Lunch time: 12:00 am to 1:00 pm Closed<br />
+                                on Sat/Sun/Holidays
+                            </span>
                         </p>
                     </div>
-                    <div className="service-text">
-                        <div>
+                    <div className="service-text flex flex-col md:flex-row gap-8">
+                        <div className="flex flex-wrap gap-8">
                             <span>About Us</span>
                             <span>Terms of service</span>
                             <span>Privacy Statement</span>
                             <span>Information Use</span>
                         </div>
-                        <div className="sns">
-                            <a href="#none"><i className="bi-facebook"></i></a>
-                            <a href="#none"><i className="bi-instagram"></i></a>
-                            <a href="#none"><i className="bi-twitter"></i></a>
-                            <a href="#none"><i className="bi-youtube"></i></a>
+                        <div className="sns flex gap-8">
+                            <a href="#none"><i className="bi-facebook text-xl"></i></a>
+                            <a href="#none"><i className="bi-instagram text-xl"></i></a>
+                            <a href="#none"><i className="bi-twitter text-xl"></i></a>
+                            <a href="#none"><i className="bi-youtube text-xl"></i></a>
                         </div>
                     </div>
                 </div>
-                <div className="sub-text">
+                <div className="sub-text border-t border-white pt-8">
                     <p>
                         Copyright © Music Streaming. All rights reserved.<br />
                         We provide services by subscribing to payment purchase safety (escrow) for safe transactions.
@@ -46,17 +44,16 @@ const Footer = () => {
                 </div>
             </div>
 
+            <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/>
 
-                <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
-                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/>
-                <Head>
-                    {/* Add the Bootstrap Icons CSS */}
-                    <link
-                        rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
-                    />
-                </Head>
-
+            <Head>
+                {/* Add the Bootstrap Icons CSS */}
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+                />
+            </Head>
         </footer>
     );
 };
